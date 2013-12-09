2954f8f27ccc8eae764d79ca418e82aa7ffa29b7
Agolveo <agolveo@gmail.com>
2013-12-09 13:42:04 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlothExpress::Application.config.secret_key_base = 'ce3632476a810d385f73be1f4d16ccd3fc826f38d1dc799dc5911963a122eb3c22663a551ab3a65d832c25ff4d124e99b476690cdc5cd9ca59c058d23bbeed3f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlothExpress::Application.config.session_store :cookie_store, key: '_sloth_express_session'
