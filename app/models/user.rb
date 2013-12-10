5bf0098196b1f81b575a8a37f9c015a05540b680
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-12-10 14:45:37 -0800

    user model passing specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ActiveRecord::Base
+  validates_presence_of :username, :email
+  validates :username, :email, uniqueness: true
+  validates_format_of :email, :with => /\w+@\w+\.\w+/i
+  validates_confirmation_of :password
+
+  has_many :products
+
+  has_secure_password
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,58 @@
 require 'spec_helper'
 
-describe "validations" do
-  let(:user) {User.new(username: “firstuser”,
-                      email: "firstuser@gmail.com")}
-  let(:user2) {User.new(username: “seconduser”,
-                      email: "seconduser@gmail.com")}
+describe User do
 
-  it "must have a username" do
-    user.username = nil
-    expect(user).to_not be_valid
-  end
+  describe "validations" do
+    let!(:user) {User.new(username: "firstuser",
+                        email: "firstuser@gmail.com",
+                        password: "123",
+                        password_confirmation: "123")}
+    let(:user2) {User.new(username: "seconduser",
+                        email: "seconduser@gmail.com",
+                        password: "456",
+                        password_confirmation: "456")}
 
-  it "must have a unique username" do
-    user2.username = "firstuser"
-    expect(user2).to_not be_valid
-  end
+      it "must have a username" do
+        user.username = nil
+        expect(user).to_not be_valid
+      end
 
-  it "must have an email" do
-    user.email = nil
-    expect(user).to_not be_valid
+      it "must have a unique username" do
+        user.save
+        user2.username = "firstuser"
+        expect(user2).to_not be_valid
+      end
+
+      it "must have an email" do
+        user.email = nil
+        expect(user).to_not be_valid
+      end
+
+      it "must have a unique email" do
+        user.save
+        user2.email = "firstuser@gmail.com"
+        expect(user2).to_not be_valid
+      end
+
+      it "must have an email with dot" do
+        user.email = "firstuser@gmailcom"
+        expect(user).to_not be_valid
+      end
+
+      it "must have an email with at symbol" do
+        user.email = "firstusergmail.com"
+        expect(user).to_not be_valid
+      end
+
+      it "must accept valid email" do
+        expect(user).to be_valid
+      end
+
+      it "must have password confirmation be the same as password" do
+        user.password = "123"
+        user.password_confirmation = "456"
+        expect(user).to_not be_valid
+      end
+
   end
 end
