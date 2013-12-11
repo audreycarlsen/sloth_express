eb161e311cbc170bcb783d11f8e68988f6619394
Agolveo <agolveo@gmail.com>
2013-12-10 23:44:23 -0800

    updated controller methods and form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,28 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created'}
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new'}
+        format.json { render json: @user.errors, status: :unprocessable_entity}
+      end
+    end
   end
 
   def update
   end
 
   def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html {redirect_to users_url}
+      format.json { head :no_content}
+    end
   end
 
   private
@@ -28,6 +44,6 @@ class UsersController < ApplicationController
     end
 
     def user_params
-      params.require(:user).permit(:name, :username, :email, :password_digest)
+      params.require(:user).permit(:username, :email, :password_digest)
     end
 end
