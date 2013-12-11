8e529b2fc9d47642b26250bc709c101525b5afec
Galina <yermichevagalina@gmail.com>
2013-12-11 12:00:03 -0800

    added show view to category, added 'set_category' method to controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,6 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :edit, :update, :destroy]
+  
   def index
     @categories = Category.all
   end
@@ -23,6 +25,9 @@ class CategoriesController < ApplicationController
   end
 
 private
+def set_category
+  @category = Category.find(params[:id])
+end
 
 def category_params
   params.require(:category).permit(:name, :products)
