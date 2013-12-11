88a0f5af5c7b835ffd44e79cd786cc322cc38907
Audrey Carlson <audreycarlsen@gmail.com>
2013-12-11 11:54:46 -0800

    Added create method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,4 +12,29 @@ class ProductsController < ApplicationController
 
   def show
   end
+
+  def create
+    @product = Product.new(product_params)
+    
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
+
+
+    # if @product.save
+    #   redirect_to product_path(@product)
+    # else
+    #   render :new
+    # end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :photo, :stock)
+  end
 end
