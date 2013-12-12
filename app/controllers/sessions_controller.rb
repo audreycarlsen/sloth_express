3f00bbca9f1cec66189f08436af4e33d7859f5e9
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-12-12 12:16:20 -0800

    Log in and out completed, login status bar added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to products_path, :notice => "Welcome, Sloth Cadet #{user.username}! Your mission, should you choose to accept it, is to find the best in sloth themed products and values!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to products_path, :notice => "Bye, Sloth Cadet, hope to see you soon!"
   end
 end
