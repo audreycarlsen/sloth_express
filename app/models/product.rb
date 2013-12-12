6c9b67a251d760b872b7476cebd40bb977a6a953
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-12 11:14:34 -0800

    Added validation to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: {:greater_than: true}
+  validates :user_id, presence: true
+
   has_many :product_categories
   has_many :categories, through: :product_categories
+  belongs_to :user
 end
