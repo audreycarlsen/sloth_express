06edb265b43e8ed55d929c1dbd2a6392d620e0c3
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-12-13 13:53:44 -0800

    New users log in automatically

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,6 +24,7 @@ class UsersController < ApplicationController
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created'}
         format.json { render action: 'show', status: :created, location: @user }
+        session[:user_id] = @user.id
       else
         format.html { render action: 'new'}
         format.json { render json: @user.errors, status: :unprocessable_entity}
