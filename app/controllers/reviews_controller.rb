1bfc92d26cc72d152a22ab0085a8da92ab18e1a5
Agolveo <agolveo@gmail.com>
2013-12-13 16:08:17 -0800

    commit to pull

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,7 @@
 class ReviewsController < ApplicationController
   
   def create
-  	@product = Product.find(params[:product_id])
-
-  	@review = @product.reviews.create(review_params)
-
-  	
+  	Review.create_review
 
   	redirect_to product_path(@product)
   end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :product
+
+  def create_review
+  	@product = Product.find(params[:product_id])
+
+  	@review = @product.reviews.create(review_params)
+  end
 end
