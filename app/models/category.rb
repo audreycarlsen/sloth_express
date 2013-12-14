b94ae251f4d037c3073645bf33c3bdeb2ca65c3b
Galina <yermichevagalina@gmail.com>
2013-12-13 22:35:38 -0800

    added category validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_many :product_categories
   has_many :products, through: :product_categories
   
-  validates :category,  uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
