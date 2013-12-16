2d9930f36bd1ccbdce42623f6dc7cdde0af9b051
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-16 11:11:51 -0800

    Link reviews to user, user can't review own product

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
   belongs_to :product
 
-   validates_presence_of :title, :author, :content, :rating
+  validates_presence_of :title, :author, :content, :rating
+  validates :rating, numericality: {:in => 1..5}
 end
