e860e07b487072e9a3e8a866c4ef1a65d27081a9
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-16 13:45:15 -0800

    Created Purchase model

---
+++ db/migrate/20131216214416_create_purchases.rb
@@ -0,0 +1,16 @@
+class CreatePurchases < ActiveRecord::Migration
+  def change
+    create_table :purchases do |t|
+      t.string :order_id
+      t.string :email
+      t.string :address
+      t.string :name
+      t.string :cc_number
+      t.string :cc_expiration
+      t.string :cvv
+      t.string :zipcode
+
+      t.timestamps
+    end
+  end
+end
