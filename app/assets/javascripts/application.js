cd4943a00b21a59f2687721d70e4022a6b8b1a05
Galina <yermichevagalina@gmail.com>
2013-12-17 13:57:47 -0800

    moved javascript code to application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //require turbolinks
 //= require_tree .
+
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
