c202db02405336f4a7e692d90125fb9ad9ea1d5c
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-16 17:03:47 -0800

    Added a minor comment

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,2 +1,11 @@
 class Purchase < ActiveRecord::Base
+  validates :email, presence: true, format: {with: /\w+@\w+\.\w+/i}
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :cc_number, presence: true
+  validates :cc_expiration, presence: true
+  validates :cvv, presence: true
+  validates :zipcode, presence: true
+
+
 end
