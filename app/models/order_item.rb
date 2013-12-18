bd80dad93121d18899acf14aab10169c33ecf3e3
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-12-17 16:24:52 -0800

    validation maybe

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,4 +4,11 @@ class OrderItem < ActiveRecord::Base
 
   # validates :quantity, presense :true
   # validates :quantity, presence :true, numericality: {:greater_than => 0}
+  validate :order_item_quantity_cannot_be_greater_than_product_stock
+
+  def order_item_quantity_cannot_be_greater_than_product_stock
+    if quantity > product.stock
+      errors.add(:quantity, "can't be greater than number in stock")
+    end
+  end
 end
