14ca9dafbb55f306898a36bfc90c59455e524500
Agolveo <agolveo@gmail.com>
2013-12-18 22:04:14 -0800

    working on user sold items

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,8 @@ SlothExpress::Application.routes.draw do
   resources :purchases
   resources :categories
 
+  get "users" => "users#orders"
+
   get "log_out" => "sessions#destroy", :as => "log_out"
   get "log_in"  => "sessions#new",     :as => "log_in"
   get "sign_up" => "users#new",        :as => "sign_up"
