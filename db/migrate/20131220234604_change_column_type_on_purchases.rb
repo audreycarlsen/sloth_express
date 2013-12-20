f1a3b8038cd496314f32ab3670170701551fdaba
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-20 15:57:29 -0800

    kerri debugging

--- db/migrate/20131220234604_change_column_type_on_purchases.rb
+++ db/migrate/20131220234604_change_column_type_on_purchases.rb
@@ -1,5 +1,7 @@
 class ChangeColumnTypeOnPurchases < ActiveRecord::Migration
   def change
-    change_column :purchases, :order_id, :integer
+    if !Rails.env.producton?
+      change_column :purchases, :order_id, :integer
+    end
   end
 end
