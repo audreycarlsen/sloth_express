59fc94b87fee7f18ef167378a9a59e9633f49841
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-20 15:55:42 -0800

    Kerri's custom SQL

---
+++ db/migrate/20131220235211_manual_sql_by_kerri_for_the_lols.rb
@@ -0,0 +1,11 @@
+class ManualSqlByKerriForTheLols < ActiveRecord::Migration
+  def change
+    if Rails.env.producton?
+      connection.execute(%q{
+        alter table puchases
+        alter column order_id
+        type integer using cast(order_id as integer)
+      })
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131220234604) do
+ActiveRecord::Schema.define(version: 20131220235211) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
