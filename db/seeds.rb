5f175ec99267eae09327311e4914c08ab7f9e83a
Elizabeth Uselton <elizabeth.uselton@gmail.com>
2013-12-20 15:05:56 -0800

    Added commented out seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# users = User.create([
+#   {user_name: 'Audrey', email: 'audrey@audrey.com', password: 'password', password_confirmation: 'password'}, 
+#   {user_name: 'Elizabeth', email: 'elizabeth@elizabeth.com', password: 'password', password_confirmation: 'password'}, 
+#   {user_name: 'Galina', email: 'galina@galina.com', password: 'password', password_confirmation: 'password'}, 
+#   {user_name: 'Asha', email: 'asha@asha.com', password: 'password', password_confirmation: 'password'},
+#   {user_name: 'Sloth King', email: 'sloth@sloth.com', password: 'password', password_confirmation: 'password'}])
+
+# categories = Category.create([{category: 'Apparel'}, {category: 'Animals'}, {category: 'Novelty Items'},{category: 'Toys'},{category: 'Food'},{category: 'Literature'}])
+
+# products = Product.create([
+#   {name: 'Luna Sandals', description: 'It represents the best fruits of our experiences and experiments with old-school footwear and gleanings from insights that we have gathered from both our ancestors and our customers.', price: 65.00, photo: 'https://s3.amazonaws.com/lunastore/images/277/full.jpg?1357670041', user_id: 1, stock: 20, item_status: 'active'}, {name: 'Bookis', description: 'Yeah, you could do it that way.', price: 3443647.00, photo: '', user_id: 2, stock: 1, item_status: 'active'}, {name: 'Bookis', description: 'Yeah, you could do it that way.', price: 3443647.00, photo: '', user_id: 2, stock: 1, item_status: 'active'}])
