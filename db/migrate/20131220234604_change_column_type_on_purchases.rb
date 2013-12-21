0bae35630de9fd5cf36fec12d598e2f3ace24af4
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-20 16:01:52 -0800

    kerri messing with migrations

--- db/migrate/20131220234604_change_column_type_on_purchases.rb
+++ db/migrate/20131220234604_change_column_type_on_purchases.rb
@@ -1,7 +1,7 @@
 class ChangeColumnTypeOnPurchases < ActiveRecord::Migration
   def change
-    if !Rails.env.producton?
-      change_column :purchases, :order_id, :integer
-    end
+    # if !Rails.env.producton?
+    #   change_column :purchases, :order_id, :integer
+    # end
   end
 end
