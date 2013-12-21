2a670f4a16c63802fc6bf9a5367e06413ce67b66
Audrey Carlsen <audreycarlsen@gmail.com>
2013-12-20 16:07:43 -0800

    kerri fixing DB

--- db/migrate/20131216214416_create_purchases.rb
+++ db/migrate/20131216214416_create_purchases.rb
@@ -1,14 +1,14 @@
 class CreatePurchases < ActiveRecord::Migration
   def change
     create_table :purchases do |t|
-      t.string :order_id
-      t.string :email
-      t.string :address
-      t.string :name
-      t.string :cc_number
-      t.string :cc_expiration
-      t.string :cvv
-      t.string :zipcode
+      t.integer :order_id
+      t.string  :email
+      t.string  :address
+      t.string  :name
+      t.string  :cc_number
+      t.string  :cc_expiration
+      t.string  :cvv
+      t.string  :zipcode
 
       t.timestamps
     end
--- db/migrate/20131220234604_change_column_type_on_purchases.rb
+++ db/migrate/20131220234604_change_column_type_on_purchases.rb
@@ -1,5 +1,6 @@
 class ChangeColumnTypeOnPurchases < ActiveRecord::Migration
   def change
+    # This didn't wrk and its Kerri's fault
     # if !Rails.env.producton?
     #   change_column :purchases, :order_id, :integer
     # end
